99762a1cc53e9d9bc19c5571944baf6bebc49ec9
Maquereau2 <macrorush@gmail.com>
2024-03-18 16:28:24 +0100

    update : resize window still not working 😭

--- C aiguisé/Cmd.cs
+++ C aiguisé/Cmd.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 namespace C_aiguisé
 {
-    public class Cmd
+    public static class Cmd
     {
+    public static void test()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            /*Console.SetWindowSize(1920, 1080);*/
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            string asciiArt = File.ReadAllText("nahidwin.txt");
+
+            // Print the ASCII art to the console
+            Console.WriteLine(asciiArt);
+            /*Console.Write("Hello, World!");*/
+            Console.Read();
+            /*Console.Beep();*/
+        }
     }
 }
