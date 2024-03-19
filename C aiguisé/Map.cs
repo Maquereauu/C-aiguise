34f62c4341104e7756b0bcd42f56a7e2f1e2d1ed
ewen <ebeaufils@gaming.tech>
2024-03-19 21:15:59 +0100

    init : map

--- C aiguisé/Map.cs
+++ C aiguisé/Map.cs
@@ -7,5 +7,25 @@ namespace C_aiguisé
 {
     public class Map
     {
+        private List<Zone> _zones;
+        private Zone _currentZone;
+
+        public Map() { }    
+
+        public void AddZone(Zone zone)
+        {
+            _zones.Add(zone);
+        }
+
+        public void SetCurrentZone(Zone zone)
+        {
+            _currentZone = zone;
+        }
+
+        public void Update()
+        {
+            _currentZone.Display();
+        }
+
     }
 }
--- C aiguisé/Program.cs
+++ C aiguisé/Program.cs
@@ -7,15 +7,24 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Map map = new Map();
+        Zone zone1 = new Zone("../../../nahidwin.txt");
+        Zone zone2 = new Zone("../../../nahidwin2.txt");
+
+        map.SetCurrentZone(zone2);
         /*Cmd.test();*/
         EventManager._rightArrow += EventManager.MoveRight;
         EventManager._leftArrow += EventManager.MoveLeft;
         EventManager._downArrow += EventManager.MoveDown;
         EventManager._upArrow += EventManager.MoveUp;
 
+
+        map.Update();
+
         while (true)
         {
             EventManager.Update();
+
         }
         /*        Console.OutputEncoding = System.Text.Encoding.UTF8;
                 for (var i = 0; i <= 1000; i++)
