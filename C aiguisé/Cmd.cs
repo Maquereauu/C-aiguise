dd978c9dbc9643f8a822dd6e4ea7ac07da5e7f82
Maquereauu <gcharlesnicolai@GCFRLY145>
2024-03-19 14:55:22 +0100

    update : 😒

--- C aiguisé/Cmd.cs
+++ C aiguisé/Cmd.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -6,6 +7,80 @@ namespace C_aiguisé
 {
     public static class Cmd
     {
+/*        public static string GrayscaleImageToASCII(System.Drawing.Image img)
+        {
+            StringBuilder html = new StringBuilder();
+            Bitmap bmp = null;
+
+            try
+            {
+                // Create a bitmap from the image
+
+                bmp = new Bitmap(img);
+
+                // The text will be enclosed in a paragraph tag with the class
+
+                // ascii_art so that we can apply CSS styles to it.
+
+                html.Append("&lt;br/&rt;");
+
+                // Loop through each pixel in the bitmap
+
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        // Get the color of the current pixel
+
+                        Color col = bmp.GetPixel(x, y);
+
+                        // To convert to grayscale, the easiest method is to add
+
+                        // the R+G+B colors and divide by three to get the gray
+
+                        // scaled color.
+
+                        col = Color.FromArgb((col.R + col.G + col.B) / 3,
+                            (col.R + col.G + col.B) / 3,
+                            (col.R + col.G + col.B) / 3);
+
+                        // Get the R(ed) value from the grayscale color,
+
+                        // parse to an int. Will be between 0-255.
+
+                        int rValue = int.Parse(col.R.ToString());
+
+                        // Append the "color" using various darknesses of ASCII
+
+                        // character.
+
+                        html.Append(getGrayShade(rValue));
+
+                        // If we're at the width, insert a line break
+
+                        if (x == bmp.Width - 1)
+                            html.Append("&lt;br/&rt");
+                    }
+                }
+
+                // Close the paragraph tag, and return the html string.
+
+                html.Append("&lt;/p&rt;");
+
+                return html.ToString();
+            }
+            catch (Exception exc)
+            {
+                return exc.ToString();
+            }
+            finally
+            {
+                bmp.Dispose();
+            }
+        }*/
+
+
+
         [DllImport("user32.dll")]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
@@ -13,20 +88,20 @@ namespace C_aiguisé
         private static extern IntPtr GetConsoleWindow();
 
         const int SW_MAXIMIZE = 3;
-
+        
         public static void test()
         {
             IntPtr handle = GetConsoleWindow();
             ShowWindow(handle, SW_MAXIMIZE);
-
-            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             Console.Clear();
 
             string asciiArt = File.ReadAllText("../../../nahidwin.txt");
             Console.WriteLine(asciiArt);
             asciiArt = File.ReadAllText("../../../nahidwin2.txt");
-/*            Console.WriteLine(asciiArt);*/
+            Console.WriteLine(asciiArt);
             Console.Read();
         }
     }
--- C aiguisé/Program.cs
+++ C aiguisé/Program.cs
@@ -1,11 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using C_aiguisé;
+using Microsoft.VisualBasic;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Cmd.test();
+/*        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        for (var i = 0; i <= 1000; i++)
+        {
+            Console.Write(Strings.ChrW(i));
+            if (i % 50 == 0)
+            { // break every 50 chars
+                Console.WriteLine();
+            }
+        }
+        Console.ReadKey();*/
     }
 }
