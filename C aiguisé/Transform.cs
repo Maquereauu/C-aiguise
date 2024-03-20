56679df54c5c26ccfe4efc82c63ee52d25ab2c8d
ebeaufils <ebeaufils@GCFRLY254>
2024-03-20 12:11:25 +0100

    init : transform init : struct vect2

---
+++ C aiguisé/Transform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_aiguisé
+{
+    public class Transform
+    {
+        private Utils.vect2 _coordinates;
+        public Transform()
+        {
+            _coordinates = new Utils.vect2();
+        }
+
+        public void SetPos(int x, int y)
+        {
+            _coordinates.SetX(x);
+            _coordinates.SetX(y);
+        }
+
+        public void Translate(int x, int y)
+        {
+            _coordinates.AddX(x);
+            _coordinates.AddY(y);
+        }
+    }
+}
---
+++ C aiguisé/Utils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_aiguisé
+{
+   namespace Utils
+    {
+        struct vect2
+        {
+            int _x;
+            int _y;
+            public vect2(int x, int y)
+            {
+                _x = x; _y = y;
+            }
+            public vect2()
+            {
+                _x = 0;
+                _y = 0;
+            }
+            public int x()
+            {
+                return _x;
+            }
+            public int y()
+            {
+                return _y;
+            }
+            public void SetX(int x)
+            {
+                _x = x;
+            }
+            public void SetY(int y)
+            {
+                _y = y;
+            }
+            public void AddX(int x)
+            {
+                _x += x;
+            }
+            public void AddY(int y)
+            {
+                _y += y;
+            }
+
+        }
+    }
+}
