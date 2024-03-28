7133a6dd58db9d632955471d6711ac831b444cec
ewen <ebeaufils@gaming.tech>
2024-03-28 17:49:17 +0100

    init : dialog init : dialogManager

---
+++ C aiguisé/Source/Dialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_aiguisé
+{
+    class Dialog
+    {
+        private string _dialog;
+
+        public string _mDialog
+        {
+            get { return _dialog; }
+            set { _dialog = value; }
+        }
+        public Dialog(string dialog)
+        {
+            _dialog = dialog;
+        }
+
+        public void Display((int, int) position)
+        {
+            Console.SetCursorPosition(position.Item1, position.Item2);
+            Console.Write(_dialog);
+        }
+    }
+}
