b5f8853de0f2337e6314328a3c34f315fe44f15e
ewen <ebeaufils@gaming.tech>
2024-03-28 20:23:53 +0100

    add : test of take damage

--- TestProject1/Usings.cs
+++ TestProject1/Usings.cs
@@ -1,1 +1,32 @@
 global using NUnit.Framework;
+using C_aiguisé;
+
+namespace TestProject1
+{
+    public class Using
+    {
+
+        [Test]
+        [TestCase(100, 30, 70)]
+        [TestCase(100, 70, 30)]
+        [TestCase(100, 100, 0)]
+        [TestCase(100, 200, 0)]
+        public void TakeDamage(int hp, int damage, int result)
+        {
+            int finalHp = Tests.TakeDamage(hp, damage, result);
+            Assert.That(finalHp, Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase(100, -200, -100)]
+        public void CrashTakeDamage(int hp, int damage, int result)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                int finalHp = Tests.TakeDamage(hp, damage, result);
+                Assert.That(finalHp, Is.EqualTo(result));
+            });
+        }
+
+    }
+}
