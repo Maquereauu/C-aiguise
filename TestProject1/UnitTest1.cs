e9196c53cb4b489fdfb926587b96c88454c240ed
Maquereau2 <macrorush@gmail.com>
2024-03-28 21:08:13 +0100

    update : added UnitTest

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -51,7 +51,7 @@ namespace TestProject1
             e2._mType = 1;
             e2.TakeDamage(p1.Attack(p1._mAttackMoves[0], e2));
             Assert.That(e2._mHp, Is.EqualTo(80));
-            e2._mType = 2;
+            e2._mType = 2; 
             e2.TakeDamage(p1.Attack(p1._mAttackMoves[0], e2));
             Assert.That(e2._mHp, Is.EqualTo(0));
             e3._mDodgeChance = 100;
@@ -81,6 +81,83 @@ namespace TestProject1
 
             return player._mHp;
         }
+        [Test]
+        public static void TankAttackNormal()
+        {
+            Enemy e1 = new Enemy();
+            Weapon noeil = new Weapon("salut");
+            Tank tank = new Tank();
+            Player p1 = new Player(noeil, tank);
+            tank.setPlayer(p1);
+            tank.setAttack();
+            e1.TakeDamage(p1.Attack(p1._mAttackMoves[0], e1));
 
+            Assert.That(e1._mHp, Is.EqualTo(60));
+        }
+        [Test]
+        public static void TankAttackStrongType()
+        {
+            Enemy e1 = new Enemy();
+            Weapon noeil = new Weapon("salut");
+            Tank tank = new Tank();
+            Player p1 = new Player(noeil, tank);
+            tank.setPlayer(p1);
+            tank.setAttack();
+            e1._mType = 1;
+            p1._mCritChance = 0;
+            e1._mDodgeChance = 0;
+
+            e1.TakeDamage(p1.Attack(p1._mAttackMoves[0], e1));
+            Assert.That(e1._mHp, Is.EqualTo(80));
+        }
+        [Test]
+
+        public static void TankAttackWeakType()
+        {
+            Enemy e1 = new Enemy();
+            Weapon noeil = new Weapon("salut");
+            Tank tank = new Tank();
+            Player p1 = new Player(noeil, tank);
+            tank.setPlayer(p1);
+            tank.setAttack();
+            e1._mType = 2;
+            p1._mCritChance = 0;
+            e1._mDodgeChance = 0;
+
+            e1.TakeDamage(p1.Attack(p1._mAttackMoves[0], e1));
+            Assert.That(e1._mHp, Is.EqualTo(20));
+        }
+
+        [Test]
+        public static void TankAttackDodge()
+        {
+            Enemy e1 = new Enemy();
+            Weapon noeil = new Weapon("salut");
+            Tank tank = new Tank();
+            Player p1 = new Player(noeil, tank);
+            tank.setPlayer(p1);
+            tank.setAttack();
+            p1._mCritChance = 0;
+            e1._mDodgeChance = 100;
+
+            e1.TakeDamage(p1.Attack(p1._mAttackMoves[0], e1));
+            Assert.That(e1._mHp, Is.EqualTo(100));
+        }
+
+        [Test]
+        public static void TankAttackCrit()
+        {
+            Enemy e1 = new Enemy();
+            Weapon noeil = new Weapon("salut");
+            Tank tank = new Tank();
+            Player p1 = new Player(noeil, tank);
+            tank.setPlayer(p1);
+            tank.setAttack();
+            p1._mCritChance = 100;
+            e1._mDodgeChance = 0;
+
+            e1.TakeDamage(p1.Attack(p1._mAttackMoves[0], e1));
+            Assert.That(e1._mHp, Is.EqualTo(52));
+        }
     }
 }
